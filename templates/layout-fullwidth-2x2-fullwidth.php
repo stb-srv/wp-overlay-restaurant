f03c96ca16092d18c8745412198cb48fb88a0095
stb-srv <s.behncke@icloud.com>
2025-06-22 18:48:11 +0200

    Fix HTML rendering and grid layout

--- templates/layout-fullwidth-2x2-fullwidth.php
+++ templates/layout-fullwidth-2x2-fullwidth.php
@@ -1,8 +1,8 @@
 <div class="pm-fullwidth pm-fullwidth--top">
 <?php 
-$top_content = get_post_meta($post->ID, 'ffo_fullwidth_top', true);
-if ( trim(strip_tags($top_content)) !== '' ) {
-    echo apply_filters('the_content', $top_content);
+$top_content = get_post_meta( $post->ID, 'ffo_fullwidth_top', true );
+if ( trim( $top_content ) !== '' ) {
+    echo apply_filters( 'the_content', $top_content );
 }
 ?>
 </div>
@@ -10,9 +10,9 @@ if ( trim(strip_tags($top_content)) !== '' ) {
 <?php for ($i = 1; $i <= 4; $i++) : ?>
     <div class="pm-grid__item">
         <?php 
-        $item = get_post_meta($post->ID, "ffo_grid_item_{$i}", true);
-        if ( trim(strip_tags($item)) !== '' ) {
-            echo apply_filters('the_content', $item);
+        $item = get_post_meta( $post->ID, "ffo_grid_item_{$i}", true );
+        if ( trim( $item ) !== '' ) {
+            echo apply_filters( 'the_content', $item );
         }
         ?>
     </div>
@@ -20,9 +20,9 @@ if ( trim(strip_tags($top_content)) !== '' ) {
 </div>
 <div class="pm-fullwidth pm-fullwidth--bottom">
 <?php 
-$bottom_content = get_post_meta($post->ID, 'ffo_fullwidth_bottom', true);
-if ( trim(strip_tags($bottom_content)) !== '' ) {
-    echo apply_filters('the_content', $bottom_content);
+$bottom_content = get_post_meta( $post->ID, 'ffo_fullwidth_bottom', true );
+if ( trim( $bottom_content ) !== '' ) {
+    echo apply_filters( 'the_content', $bottom_content );
 }
 ?>
 </div>
