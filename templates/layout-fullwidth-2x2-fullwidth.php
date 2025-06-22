a40b3acdd8e9fa37b072a496e61549f35b1088de
stb-srv <s.behncke@icloud.com>
2025-06-22 18:32:56 +0200

    Abstand-bearbeitung

--- templates/layout-fullwidth-2x2-fullwidth.php
+++ templates/layout-fullwidth-2x2-fullwidth.php
@@ -1,13 +1,28 @@
 <div class="pm-fullwidth pm-fullwidth--top">
-  <?= apply_filters('the_content', get_post_meta( $post->ID, 'ffo_fullwidth_top', true ) ); ?>
+<?php 
+$top_content = get_post_meta($post->ID, 'ffo_fullwidth_top', true);
+if ( trim(strip_tags($top_content)) !== '' ) {
+    echo apply_filters('the_content', $top_content);
+}
+?>
 </div>
 <div class="pm-grid pm-grid--2x2">
-  <?php for ( $i = 1; $i <= 4; $i++ ) : ?>
+<?php for ($i = 1; $i <= 4; $i++) : ?>
     <div class="pm-grid__item">
-      <?= apply_filters( 'the_content', get_post_meta( $post->ID, "ffo_grid_item_{$i}", true ) ); ?>
+        <?php 
+        $item = get_post_meta($post->ID, "ffo_grid_item_{$i}", true);
+        if ( trim(strip_tags($item)) !== '' ) {
+            echo apply_filters('the_content', $item);
+        }
+        ?>
     </div>
-  <?php endfor; ?>
+<?php endfor; ?>
 </div>
 <div class="pm-fullwidth pm-fullwidth--bottom">
-  <?= apply_filters('the_content', get_post_meta( $post->ID, 'ffo_fullwidth_bottom', true ) ); ?>
+<?php 
+$bottom_content = get_post_meta($post->ID, 'ffo_fullwidth_bottom', true);
+if ( trim(strip_tags($bottom_content)) !== '' ) {
+    echo apply_filters('the_content', $bottom_content);
+}
+?>
 </div>
