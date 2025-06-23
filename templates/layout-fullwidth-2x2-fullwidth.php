8ab5c455bd0dce38532cceb506452192d6147541
stb-srv <s.behncke@icloud.com>
2025-06-23 12:08:15 +0200

    Add grid item headings and improve editing

--- templates/layout-fullwidth-2x2-fullwidth.php
+++ templates/layout-fullwidth-2x2-fullwidth.php
@@ -9,7 +9,11 @@ if ( trim( $top_content ) !== '' ) {
 <div class="pm-grid pm-grid--2x2">
 <?php for ($i = 1; $i <= 4; $i++) : ?>
     <div class="pm-grid__item">
-        <?php 
+        <?php
+        $heading = get_post_meta( $post->ID, "ffo_grid_heading_{$i}", true );
+        if ( trim( $heading ) !== '' ) {
+            echo '<h3 class="ffo-grid-heading">' . esc_html( $heading ) . '</h3>';
+        }
         $item = get_post_meta( $post->ID, "ffo_grid_item_{$i}", true );
         if ( trim( $item ) !== '' ) {
             echo apply_filters( 'the_content', $item );
