89740fa249af0f66ec87ef93b6260b7c2a3700bd
stb-srv <s.behncke@icloud.com>
2025-06-24 19:27:39 +0200

    Allow per-tile title styling

--- assets/overlay.js
+++ assets/overlay.js
@@ -18,7 +18,15 @@
       tile1TitleSize: '',
       tile2TitleSize: '',
       tile3TitleSize: '',
-      tile4TitleSize: ''
+      tile4TitleSize: '',
+      tile1TitleColor: '',
+      tile2TitleColor: '',
+      tile3TitleColor: '',
+      tile4TitleColor: '',
+      tile1TitleFont: '',
+      tile2TitleFont: '',
+      tile3TitleFont: '',
+      tile4TitleFont: ''
     }, params || {});
 
     var overlay = document.createElement('div');
@@ -27,6 +35,14 @@
     if(opts.tileTitleSize) overlay.style.setProperty('--tile-title-size', opts.tileTitleSize);
     if(opts.tileTextSize) overlay.style.setProperty('--tile-text-size', opts.tileTextSize);
 
+    function buildStyle(size, color, font){
+      var s = '';
+      if(size) s += 'font-size:' + size + ';';
+      if(color) s += 'color:' + color + ';';
+      if(font) s += 'font-family:' + font + ';';
+      return s ? ' style="' + s + '"' : '';
+    }
+
     overlay.innerHTML =
       '<div class="ffo-overlay__inner">' +
         '<button class="ffo-overlay__close" aria-label="Close Overlay">&times;</button>' +
@@ -34,25 +50,25 @@
         '<div class="ffo-overlay__grid">' +
           '<div class="ffo-overlay__tile">' +
             '<h3 class="ffo-overlay__tile-title"' +
-              (opts.tile1TitleSize ? ' style="font-size:' + opts.tile1TitleSize + ';"' : '') +
+              buildStyle(opts.tile1TitleSize, opts.tile1TitleColor, opts.tile1TitleFont) +
             '>' + opts.tile1Title + '</h3>' +
             '<p class="ffo-overlay__tile-text">' + opts.tile1Text + '</p>' +
           '</div>' +
           '<div class="ffo-overlay__tile">' +
             '<h3 class="ffo-overlay__tile-title"' +
-              (opts.tile2TitleSize ? ' style="font-size:' + opts.tile2TitleSize + ';"' : '') +
+              buildStyle(opts.tile2TitleSize, opts.tile2TitleColor, opts.tile2TitleFont) +
             '>' + opts.tile2Title + '</h3>' +
             '<p class="ffo-overlay__tile-text">' + opts.tile2Text + '</p>' +
           '</div>' +
           '<div class="ffo-overlay__tile">' +
             '<h3 class="ffo-overlay__tile-title"' +
-              (opts.tile3TitleSize ? ' style="font-size:' + opts.tile3TitleSize + ';"' : '') +
+              buildStyle(opts.tile3TitleSize, opts.tile3TitleColor, opts.tile3TitleFont) +
             '>' + opts.tile3Title + '</h3>' +
             '<p class="ffo-overlay__tile-text">' + opts.tile3Text + '</p>' +
           '</div>' +
           '<div class="ffo-overlay__tile">' +
             '<h3 class="ffo-overlay__tile-title"' +
-              (opts.tile4TitleSize ? ' style="font-size:' + opts.tile4TitleSize + ';"' : '') +
+              buildStyle(opts.tile4TitleSize, opts.tile4TitleColor, opts.tile4TitleFont) +
             '>' + opts.tile4Title + '</h3>' +
             '<p class="ffo-overlay__tile-text">' + opts.tile4Text + '</p>' +
           '</div>' +
@@ -103,7 +119,15 @@
       tile1TitleSize: d.tile1TitleSize || '',
       tile2TitleSize: d.tile2TitleSize || '',
       tile3TitleSize: d.tile3TitleSize || '',
-      tile4TitleSize: d.tile4TitleSize || ''
+      tile4TitleSize: d.tile4TitleSize || '',
+      tile1TitleColor: d.tile1TitleColor || '',
+      tile2TitleColor: d.tile2TitleColor || '',
+      tile3TitleColor: d.tile3TitleColor || '',
+      tile4TitleColor: d.tile4TitleColor || '',
+      tile1TitleFont: d.tile1TitleFont || '',
+      tile2TitleFont: d.tile2TitleFont || '',
+      tile3TitleFont: d.tile3TitleFont || '',
+      tile4TitleFont: d.tile4TitleFont || ''
     });
   };
 })();
